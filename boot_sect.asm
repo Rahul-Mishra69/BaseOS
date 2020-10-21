692c143f2d5cda4f42ee35038d0d482640f72a80
Rahul Mishra <robom460@gmail.com>
2020-10-21 15:58:40 +0530

    The BIOS Prints Hello after initializing

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,6 +1,16 @@
-;infinite loop
-loop:
-    jmp loop
+mov ah,0x0e
+mov al,'H'
+int 0x10
+mov al,'e'
+int 0x10
+mov al,'l'
+int 0x10
+mov al,'l'
+int 0x10
+mov al,'o'
+int 0x10
+
+jmp $
 
 ;fill from 0-510 and add 0xaa55 in little endian mode(x86 supports little endian)
 times 510-($-$$) db 0
